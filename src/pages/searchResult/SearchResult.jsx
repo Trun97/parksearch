19260a9a9511b87d860e0a4338bb34b7a4789770
Trun97 <tarun7283@gmail.com>
2025-04-10 00:30:30 +0200

    get data

--- src/pages/searchResult/SearchResult.jsx
+++ src/pages/searchResult/SearchResult.jsx
@@ -1,6 +1,30 @@
 import './SearchResult.css'
+import {useContext, useEffect, useState } from "react";
+import axios from "axios";
+import { ParkSearchContext} from "../../context/ParkSearchContext/ParkSearchContext.jsx";
 
 function SearchResult() {
+    const { selectedStates, selectedFacilities } = useContext(ParkSearchContext);
+    const [loading, setLoading] = useState(true);
+    const [error, setError] = useState("");
+
+    useEffect(() => {
+        async function fetchParks() {
+            setLoading(true)
+            try {
+                const response2 = await axios.get( `https://developer.nps.gov/api/v1/amenities/parksplaces?stateCode=${selectedStates.join(",")}&id=${selectedFacilities.join(",")}&api_key=${import.meta.env.VITE_NPS_API_KEY}`);
+                console.log(response2.data.data);
+            } catch(err) {
+                console.error("fout bij data ophalen", err)
+                setError("iets ging niet goed")
+            } finally {
+                setLoading(false);
+            }
+        }
+
+        fetchParks();
+    }, [selectedStates, selectedFacilities]);
+
     return <h1>Search Result</h1>;
 }
 
