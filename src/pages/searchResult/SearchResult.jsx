f887d6364e49877de613155c61726c171c51b928
Trun97 <tarun7283@gmail.com>
2025-04-10 22:30:13 +0200

    filter parks by selected states

--- src/pages/searchResult/SearchResult.jsx
+++ src/pages/searchResult/SearchResult.jsx
@@ -11,7 +11,8 @@ function SearchResult() {
 
     useEffect(() => {
         async function fetchParks() {
-            setLoading(true)
+            setLoading(true);
+            setError("");
             try {
                 const response2 = await axios.get(`https://developer.nps.gov/api/v1/amenities/parksplaces?stateCode=${selectedStates.join(",")}&id=${selectedFacilities.join(",")}&api_key=${import.meta.env.VITE_NPS_API_KEY}`);
                 console.log(response2.data.data);
@@ -25,10 +26,45 @@ function SearchResult() {
                         }
                     })
                 })
+                console.log(parkCodes);
+                if (parkCodes.length === 0) {
+                    setError("No parks found for this selection.");
+                    setParks([]);
+                    setLoading(false);
+                }
+                const res2 = await axios.get(
+                    `https://developer.nps.gov/api/v1/parks?parkCode=${parkCodes.join(",")}&api_key=${import.meta.env.VITE_NPS_API_KEY}`
+                );
+
+                const allParks = res2.data.data;
+                const filtered = [];
+
+                console.log(allParks);
+
+                for (let i = 0; i < allParks.length; i++) {
+                    const park = allParks[i];
+                    const parkStateString = park.states;
+                    const parkStateArray = parkStateString.split(",");
+
+                    for (let j = 0; j < parkStateArray.length; j++) {
+                        const state = parkStateArray[j];
+
+                        if (selectedStates.includes(state)) {
+                            filtered.push(park);
+                        }
+                    }
+                }
+
+                if (filtered.length === 0) {
+                    setError("No parks found in the selected states.");
+                }
+
+                setParks(filtered);
+                console.log(parks);
 
             } catch (err) {
-                console.error("fout bij data ophalen", err)
-                setError("iets ging niet goed")
+                console.error("error fetching data:", err)
+                setError("something went wrong, try a different selection")
             } finally {
                 setLoading(false);
             }
@@ -37,9 +73,12 @@ function SearchResult() {
         fetchParks();
     }, [selectedStates, selectedFacilities]);
 
+    if (loading) return <p>Loading...</p>;
+
     return (
         <>
             <h1>Search Result</h1>
+            {loading && <p>Loading...</p>}
             {error && <p>{error}</p>}
 
             {parks.length > 0 && (
