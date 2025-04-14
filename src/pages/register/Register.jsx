c024e1064df7a615609880ac29da50eb71398ed3
Trun97 <tarun7283@gmail.com>
2025-04-14 17:16:31 +0200

    handleChange and handleSubmit for register

--- src/pages/register/Register.jsx
+++ src/pages/register/Register.jsx
@@ -1,23 +1,60 @@
 import './Register.css'
+import { useState } from "react";
 
 function Register() {
+    const [formData, setFormData] = useState({
+        username: "",
+        email: "",
+        password: "",
+    });
+
+    function handleChange(e) {
+        setFormData({
+            ...formData,
+            [e.target.name]: e.target.value,
+        });
+    }
+
+    function handleSubmit(e) {
+        e.preventDefault();
+        console.log("Formulier verzonden!");
+        console.log("Gebruikersnaam:", formData.username);
+        console.log("Email:", formData.email);
+        console.log("Wachtwoord:", formData.password);
+    }
+
     return (
         <div>
             <h1>Register</h1>
-            <form>
+            <form onSubmit={handleSubmit}>
                 <label>
                     Username:
-                    <input type="text" name="username" />
+                    <input
+                        type="text"
+                        name="username"
+                        value={formData.username}
+                        onChange={handleChange}
+                    />
                 </label>
                 <br />
                 <label>
                     Email:
-                    <input type="email" name="email" />
+                    <input
+                        type="email"
+                        name="email"
+                        value={formData.email}
+                        onChange={handleChange}
+                    />
                 </label>
                 <br />
                 <label>
                     Password:
-                    <input type="password" name="password" />
+                    <input
+                        type="password"
+                        name="password"
+                        value={formData.password}
+                        onChange={handleChange}
+                    />
                 </label>
                 <br />
                 <button type="submit">Create account</button>
