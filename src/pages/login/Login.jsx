b2c6a44d02b71e9d8058036b64d26f1ee0d22a02
Trun97 <tarun7283@gmail.com>
2025-04-15 15:09:27 +0200

    setup loginpage

--- src/pages/login/Login.jsx
+++ src/pages/login/Login.jsx
@@ -1,7 +1,47 @@
 import './Login.css'
+import { useState } from "react";
 
-function Login(){
-    return <h1>Login</h1>;
+function Login() {
+
+    const [username, setUsername] = useState("");
+    const [password, setPassword] = useState("");
+
+    function handleUsernameChange(e) {
+        setUsername(e.target.value);
+        console.log("Gebruikersnaam:", e.target.value);
+    }
+
+    function handlePasswordChange(e) {
+        setPassword(e.target.value);
+        console.log("Wachtwoord:", e.target.value);
+    }
+
+    return (
+        <div>
+            <h1>Login</h1>
+            <form>
+                <label>
+                    Username:
+                    <input
+                        type="text"
+                        value={username}
+                        onChange={handleUsernameChange}
+                    />
+                </label>
+                <br />
+                <label>
+                    Password:
+                    <input
+                        type="password"
+                        value={password}
+                        onChange={handlePasswordChange}
+                    />
+                </label>
+                <br />
+                <button type="submit">Login</button>
+            </form>
+        </div>
+    );
 }
 
 export default Login;
