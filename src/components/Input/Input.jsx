fc85cfa9bcb9c0d9ebc84144cc937abb32502acc
Trun97 <tarun7283@gmail.com>
2025-04-17 20:46:30 +0200

    styling navbar, footer, input and button components

--- src/components/Input/Input.jsx
+++ src/components/Input/Input.jsx
@@ -3,6 +3,7 @@ import './Input.css'
 function Input({ type = "text", name, value, onChange }) {
     return (
         <input
+            className="input"
             type={type}
             name={name}
             value={value}
